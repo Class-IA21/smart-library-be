086d4a80702030098fcdd06b8ca1ccb2c15d052b
Dimas Febriyanto <dimassfeb@gmail.com>
2024-05-24 21:11:17 +0700

    add: smart_library.sql

--- smart_library.sql
+++ smart_library.sql
@@ -1,116 +1,174 @@
--- MySQL dump 10.13  Distrib 8.3.0, for macos14.2 (arm64)
+-- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
--- Host: localhost    Database: smart_library
+-- Host: 127.0.0.1    Database: smart_library
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	8.0.35-0ubuntu0.22.04.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+/*!50503 SET NAMES utf8mb4 */
+;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
+;
+/*!40103 SET TIME_ZONE='+00:00' */
+;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */
+;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
+;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
+;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
+;
 
 --
 -- Table structure for table `books`
 --
 
 DROP TABLE IF EXISTS `books`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+
 CREATE TABLE `books` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `publisher` varchar(255) NOT NULL,
-  `published_date` date NOT NULL,
-  `isbn` varchar(20) NOT NULL,
-  `pages` int NOT NULL,
-  `language` varchar(50) NOT NULL,
-  `genre` varchar(100) NOT NULL,
-  `description` text,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `card_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_card_id` (`card_id`),
-  CONSTRAINT `fk_card_id` FOREIGN KEY (`card_id`) REFERENCES `card_rfid` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    `id` int NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `author` varchar(255) NOT NULL,
+    `publisher` varchar(255) NOT NULL,
+    `published_date` date NOT NULL,
+    `isbn` varchar(20) NOT NULL,
+    `pages` int NOT NULL,
+    `language` varchar(50) NOT NULL,
+    `genre` varchar(100) NOT NULL,
+    `description` text,
+    `card_id` int DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_card_id` (`card_id`),
+    CONSTRAINT `fk_card_id` FOREIGN KEY (`card_id`) REFERENCES `card_rfid` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 36 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 
 --
 -- Dumping data for table `books`
 --
 
-LOCK TABLES `books` WRITE;
-/*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (1,'The Great Gatsby','F. Scott Fitzgerald','Charles Scribner\'s Sons','1925-04-10','9780743273565',180,'English','Novel','A novel set in the 1920s that tells the story of Jay Gatsby and his unrequited love for Daisy Buchanan.','2024-05-18 16:25:06','2024-05-21 04:37:38',1);
-/*!40000 ALTER TABLE `books` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */
+;
+
+--
+-- Table structure for table `borrows`
+--
+
+DROP TABLE IF EXISTS `borrows`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+
+CREATE TABLE `borrows` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `book_id` int DEFAULT NULL,
+    `student_id` int DEFAULT NULL,
+    `transaction_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+    `borrow_date` datetime DEFAULT CURRENT_TIMESTAMP,
+    `due_date` datetime DEFAULT NULL,
+    `return_date` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_book` (`book_id`),
+    KEY `fk_student` (`student_id`),
+    CONSTRAINT `fk_book` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+    CONSTRAINT `fk_student` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 19 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
+
+--
+-- Dumping data for table `borrows`
+--
+
+/*!40000 ALTER TABLE `borrows` DISABLE KEYS */
+;
 
 --
 -- Table structure for table `card_rfid`
 --
 
 DROP TABLE IF EXISTS `card_rfid`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+
 CREATE TABLE `card_rfid` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `uid` varchar(100) DEFAULT NULL,
-  `type` enum('book','student') DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    `id` int NOT NULL AUTO_INCREMENT,
+    `uid` varchar(100) DEFAULT NULL,
+    `type` enum('book', 'student') DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 30 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 
 --
 -- Dumping data for table `card_rfid`
 --
 
-LOCK TABLES `card_rfid` WRITE;
-/*!40000 ALTER TABLE `card_rfid` DISABLE KEYS */;
-INSERT INTO `card_rfid` VALUES (1,'176237163123617263','book');
-/*!40000 ALTER TABLE `card_rfid` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `card_rfid` DISABLE KEYS */
+;
 
 --
 -- Table structure for table `students`
 --
 
 DROP TABLE IF EXISTS `students`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
+
 CREATE TABLE `students` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT NULL,
-  `npm` varchar(8) DEFAULT NULL,
-  `card_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_student_card_id` (`card_id`),
-  CONSTRAINT `fk_student_card_id` FOREIGN KEY (`card_id`) REFERENCES `card_rfid` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(30) DEFAULT NULL,
+    `npm` varchar(8) DEFAULT NULL,
+    `card_id` int DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_student_card_id` (`card_id`),
+    CONSTRAINT `fk_student_card_id` FOREIGN KEY (`card_id`) REFERENCES `card_rfid` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 
 --
 -- Dumping data for table `students`
 --
 
-LOCK TABLES `students` WRITE;
-/*!40000 ALTER TABLE `students` DISABLE KEYS */;
-/*!40000 ALTER TABLE `students` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */
+;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Dumping routines for database 'smart_library'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */
+;
 
--- Dump completed on 2024-05-21 13:16:39
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */
+;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */
+;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */
+;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
+;
+
+-- Dump completed on 2024-05-24 21:09:40
